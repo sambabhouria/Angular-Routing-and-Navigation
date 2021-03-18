41bd4d7a6fab2de779bfcf014c672dc063210c8c
Samba Diallo <samba.diallo@id-mac007.idtgv.cnit>
2021-03-18 15:57:21 +0100

    Wildcard Route and Redirecting Routes

--- routing-navigation/src/app/app-routing.module.ts
+++ routing-navigation/src/app/app-routing.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DepartmentListComponent } from './department-list/department-list.component';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
+  {path:'', redirectTo: '/departments', pathMatch:'full'},
   {path:'departments', component: DepartmentListComponent },
-  {path:'employees', component: EmployeeListComponent }
+  {path:'employees', component: EmployeeListComponent },
+  // this will always be last configuration in the array that because tru to match
+  // router from the top
+  {path:'**', component: PageNotFoundComponent}
 ];
 
 @NgModule({
@@ -18,7 +23,7 @@ export const routingComponents = [
   DepartmentListComponent,
   // DepartmentDetailComponent,
   EmployeeListComponent,
-  // PageNotFoundComponent,
+  PageNotFoundComponent,
   // DepartmentOverviewComponent,
   // DepartmentContactComponent
 ]
